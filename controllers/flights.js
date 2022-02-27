69d7ca86fec3973f4fc4c09e4b0d4c351d8aa0b6
bluesnake0818 <raidient87@gmail.com>
2022-02-28 07:53:18 +0900

    create add new flight feature

---
+++ controllers/flights.js
@@ -0,0 +1,39 @@
+import { Flight } from "../models/flight.js"
+
+function newFlight(req, res) {
+  res.render('flights/new')
+}
+
+function create(req, res){
+  console.log("req.body before", req.body)
+  for (let key in req.body) {
+    if(req.body[key] === "") delete req.body[key]
+  }
+  const flight = new Flight(req.body)
+  console.log(flight)
+  flight.save(function(err) {
+    // revisit to fix the error handler
+    if (err) return res.redirect('/flights/new')
+    // revisit to redirect to /flights/index
+    res.redirect('/flights/new')
+  })
+}
+
+function index(req, res) {
+  Flight.find({}, function (error, flights) {
+    console.log(error)
+    res.render("flights/index", {
+      error: error,
+      flights: flights,
+    })
+  })
+}
+
+
+
+export {
+  newFlight as new, 
+  create,
+  index,
+
+}
--- models/flight.js
+++ models/flight.js
@@ -3,9 +3,36 @@ import mongoose from 'mongoose'
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 
-const movieSchema = new Schema({
-  airline: String,
-  airport: String,
-  flightNo: Number,
-  departs: Date,
+// const schemaOptions = {
+//   timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+// };
+
+const flightSchema = new Schema({
+  airline: {
+    type: String,
+    enum: ["American", "Southwest", "United"],
+  },
+  airport: {
+    type: String,
+    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+    default: 'DEN'
+  },
+  flightNo: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 9999
+  },
+  departs: {
+    type: Date,
+    default: () => new Date(+new Date() + 365*24*60*60*1000)
+  }
+}, {
+    timestamps: true  
 })
+
+const Flight = mongoose.model('Flight', flightSchema)
+
+export {
+  Flight
+}
