9fb3a58079e59a2c570b5550929449d722ab3b25
bluesnake0818 <raidient87@gmail.com>
2022-03-03 07:33:50 +0900

    stubbed up function create for meals.js

--- controllers/meals.js
+++ controllers/meals.js
@@ -2,14 +2,19 @@ import { Meal } from '../models/meal.js'
 
 function newMeal(req, res) {
   // console.log("sanity check")
-  Meal.find({}, function (err, meal) {
-    res.render('meal/new', {
+  Meal.find({}, function (err, meals) {
+    res.render('meals/new', {
       title: 'Add Meal', 
       meals: meals,
     })
   })
 }
 
+function create(req, res) {
+  console.log("sanity check")
+}
+
 export {
   newMeal as new,
+  create,
 }
